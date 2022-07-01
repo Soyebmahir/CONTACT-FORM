37218c50e31a3dfa63d4845c613fe17e9cfb0b76
Soyeb <tssoyeb@gmail.com>
2022-07-02 01:49:15 +0600

    Left side of contact page

---
+++ src/contact/index.js
@@ -0,0 +1,41 @@
+import React from "react"; 
+import { Container, Row, Col } from "react-bootstrap";
+import { contactConfig } from "../content_option"
+
+export default function ContactUs() {
+  
+
+  return (
+      <Container>
+     
+        <Row className="mb-5 mt-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Contact Me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5" className="mb-5">
+            <h3 className="color_sec py-4">Get in touch</h3>
+            <address>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+                {contactConfig.YOUR_EMAIL}
+              </a>
+              <br />
+              <br />
+              {contactConfig.hasOwnProperty("YOUR_FONE") ? (
+                <p>
+                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+                </p>
+              ) : (
+                ""
+              )}
+            </address>
+            <p>{contactConfig.description}</p>
+          </Col>
+        
+        </Row>
+      </Container>
+  );
+}
---
+++ src/content_option.js
@@ -0,0 +1,14 @@
+const contactConfig = {
+  YOUR_EMAIL: 'tssoyeb@gmail.com',
+  YOUR_FONE: '+8801645249676',
+  description:
+    'I’m really excited to create  the best and do something exceptional which will be helpful for the company. I’m experienced in designing and developing some web with the focus on a user-friendly interface. ',
+ 
+};
+
+
+export {
+
+  contactConfig,
+
+};
